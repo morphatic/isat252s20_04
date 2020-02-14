2ace2894169f4875b6e064d48027270047c71c98
Morgan Benton <registered@morphatic.com>
2020-02-14 14:19:12 -0500

    add fizz test

--- python/fizzbuzz/fizzbuzz.py
+++ python/fizzbuzz/fizzbuzz.py
@@ -1,5 +1,14 @@
 """A FizzBuzz program"""
 
+# import necessary supportin libraries
+from numbers import Number
+
 def fizz(x):
   """Checks to see if the input is a multiple of 3"""
-  return x % 3 == 0
+  # check to see if the input is a number
+  if isinstance(x, Number):
+    # why yes, it is a number
+    return x % 3 == 0
+  else:
+    # nope, it is NOT a number
+    return False
--- python/fizzbuzz/fizzbuzz_test.py
+++ python/fizzbuzz/fizzbuzz_test.py
@@ -13,5 +13,12 @@ def describe_a_fizzbuzz_program_that():
 
   def can_indicate_if_a_number_is_a_multiple_of_3():
     """Checks to see if a number is a multiple of 3"""
-    assert fizz(3) == True
-    assert fizz(4) == False
+    assert fizz(3) == True       # multiple of 3
+    assert fizz(4) == False      # non-multiple of 3
+    assert fizz(0) == True       # zero
+    assert fizz(15) == True      # multiple of 3 and 5
+    assert fizz(-3) == True      # negative multiple of 3
+    assert fizz(-2) == False     # negative non-multiple of 3
+    assert fizz(3.5) == False    # non-integer
+    assert fizz('buzz') == False # non-numeric
+    assert fizz() == False       # no input
