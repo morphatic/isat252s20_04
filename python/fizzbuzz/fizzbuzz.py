c038928429926b5b353c07de5d377d4751f4f290
Morgan Benton <registered@morphatic.com>
2020-02-24 14:16:17 -0500

    fizzbuzz() test done

--- python/fizzbuzz/fizzbuzz.py
+++ python/fizzbuzz/fizzbuzz.py
@@ -1,15 +1,28 @@
 """A FizzBuzz program"""
 
-# import necessary supportin libraries
+# import necessary supporting libraries
 from numbers import Number
 
 def fizz(x):
   """
     Checks to see if the input `x` is numeric and a multiple of 3.
-    If it is, it outputs 'fizz'.
+    If it is, it outputs 'Fizz'.
     Otherwise, it outputs the input.
   """
-  if isinstance(x, Number) and x % 3 == 0:
-    return 'fizz'
-  else:
-    return x
+  return 'Fizz' if isinstance(x, Number) and x % 3 == 0 else x
+
+def buzz(x):
+  """
+    Checks to see if the input `x` is numeric and a multiple of 5.
+    If it is, it outputs 'Buzz'.
+    Otherwise, it outputs the input.
+  """
+  return 'Buzz' if isinstance(x, Number) and x % 5 == 0 else x
+
+def fibu(x):
+  """
+    Checks to see if the input `x` is numeric and a multiple of 15.
+    If it is, it outputs 'FizzBuzz'.
+    Otherwise, it outputs the input.
+  """
+  return 'FizzBuzz' if isinstance(x, Number) and x % 15 == 0 else x
---
+++ python/fizzbuzz/fizzbuzz_procedural.py
@@ -0,0 +1,12 @@
+"""
+  An example of how NOT to write fizzbuzz... maybe.
+"""
+for x in range(1, 101):
+  if x % 15 == 0:
+    print('FizzBuzz')
+  elif x % 3 == 0:
+    print('Fizz')
+  elif x % 5 == 0:
+    print('Buzz')
+  else:
+    print(x)
